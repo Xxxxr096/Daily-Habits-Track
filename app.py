a0c66db4778256dd21734f60df05830066f9d686
Amine CHABANE <amine.chabane006@gmail.com>
2025-07-24 08:52:09 +0200

    creation des pages index login register , et la page main. configuration de la route login et reg ister (connexion et deconexion et inscription)

--- app.py
+++ app.py
@@ -44,6 +44,11 @@ login_manager.login_view = "login"
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 """
     - Créé la table User pour l'utilisateur avec plusieurs tache 
     -> (un utilisateur peut avoir une ou plusieurs taches)
@@ -84,3 +89,58 @@ class Completion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
     habit_id = db.Column(db.Integer, db.ForeignKey("habit.id"), nullable=False)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/main")
+def main():
+    return "salut"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email=email).first()
+        if user and bycrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("main.html"))
+        else:
+            flash("Email ou mot de passe incorrect", "error")
+
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        prenom = request.form["prenom"]
+        nom = request.form["prenom"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm = request.form["confirm_password"]
+        if password != confirm:
+            flash("Les mots de passe sont différents")
+            return redirect(url_for("register"))
+
+        if User.query.filter_by(email=email):
+            flash("Un compte existe déja avec cet email.")
+            return redirect(url_for(register))
+
+        # hasher le mot de passe
+        hashed_pw = bycrypt.generate_password_hash(password).decode("utf-8")
+        # Ajouter l'utilisateur a la base de données
+        new_user = User(prenom=prenom, nom=nom, email=email, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Ton compte a bien été crée ! ", "success")
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
